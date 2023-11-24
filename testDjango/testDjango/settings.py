6cd3bc62fbf5ba7428704de4a93e5f0eeb3f2ab1
Phuongham1122 <128309100+Phuongham1122@users.noreply.github.com>
2023-11-24 17:15:33 +0700

    new commit

--- testDjango/testDjango/settings.py
+++ testDjango/testDjango/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-uyvi^%l5#%!y%@jz%&hc-$+e388k_vz*@75ut0lys@l_(ydma1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
